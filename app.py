85d23b64d56cd69575be9c5107becb5df8650bcf
Winner <usunobuwhinny55@gmail.com>
2025-07-23 17:04:54 +0100

    Added base templates and connected routes

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/log")
+def log():
+    return render_template("log.html")
+
+@app.route("/supervisor")
+def supervisor():
+    return render_template("supervisor.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
